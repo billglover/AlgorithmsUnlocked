f6aeaee4b746c297e963c2875e2d264a61396d44
Bill Glover <bill@billglover.co.uk>
2016-06-05 00:08:24 +0100

    Add heading for Recursive Binary Search

--- Algorithms.playground/Pages/Recursive Binary Search.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Recursive Binary Search.xcplaygroundpage/Contents.swift
@@ -1,4 +1,10 @@
-//: [Previous](@previous)
+/*:
+ # Recursive Binary Search
+ 
+ The following description of the recursive binary search algorithm is taken from [Algorithms Unlocked](http://amzn.to/1TTRwcs) by Thomas H. Cormen. The description has been modified slightly to align closer to the swift implementation style.
+ 
+ */
+ 
 import Foundation
 
-//: [Next](@next)
+
