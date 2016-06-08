7331c9274b0629626d519f4efa2944dac4ffc117
Bill Glover <bill@billglover.co.uk>
2016-06-08 21:47:36 +0100

    Count iterations to search over increasing array sizes

--- go/BinarySearch.go
+++ go/BinarySearch.go
@@ -3,16 +3,20 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func binarySearch(A []int, n int, x int) (int, error) {
 	p := 0
 	r := n - 1
+	iterations := 0
 
 	for p <= r {
+		iterations++
 		q := (p + r) / 2
 		if A[q] == x {
-			return q, nil
+			return iterations, nil
 		} else {
 			if A[q] > x {
 				r = q - 1
@@ -22,19 +26,37 @@ func binarySearch(A []int, n int, x int) (int, error) {
 		}
 	}
 
-	return 0, errors.New("not found")
+	return iterations, errors.New("not found")
+}
+
+func findRandomValueInArray(A []int, n int) int {
+	randomValue := rand.Intn(n)
+	iterations, _ := binarySearch(A, n, randomValue)
+	return iterations
 }
 
 func main() {
-	A := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	n := len(A)
+	// seed the random number gnerator and log the seed
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+	fmt.Printf("Random seed: %d\n", seed)
 
-	for x := 0; x < n; x++ {
-		index, err := binarySearch(A, n, x)
-		if err == nil {
-			fmt.Printf("Value %d found at index %d\n", x, index)
-		} else {
-			fmt.Printf("Value %d %s\n", x, err)
+	// generate array to search
+	fmt.Printf("Generating array to search...")
+	A := make([]int, 1000000000)
+	for i := 0; i < len(A); i++ {
+		A[i] = i
+	}
+	fmt.Printf("DONE\n")
+
+	fmt.Printf("\nArray Size | Iterations\n")
+
+	for n := 10; n <= len(A); n = n * 10 {
+		sumOfIterations := 0
+		for i := 0; i < 1000; i++ {
+			sumOfIterations += findRandomValueInArray(A, n)
 		}
+		avgIterations := sumOfIterations / 1000
+		fmt.Printf("%10d | %9d\n", n, avgIterations)
 	}
 }
