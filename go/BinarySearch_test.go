883077f3cee1cc95d7f27890ca8261080029bb7a
Bill Glover <bill@billglover.co.uk>
2016-06-09 07:16:28 +0100

    Add tests for binarySearch algorithm

---
+++ go/BinarySearch_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+	"time"
+	"math/rand"
+	"os"
+)
+
+func TestMain(m *testing.M) {
+	// seed the random number gnerator and log the seed
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+	fmt.Printf("Random seed: %d\n", seed)
+
+	os.Exit(m.Run())
+}
+
+func TestValueFound(t *testing.T) {
+	
+	A := make([]int, 100)
+	n := len(A)
+	x := rand.Intn(n)
+
+	// create an array to search
+	for i := 0; i < n; i++ {
+		A[i] = i
+	}
+
+	// find our value	
+	_, err := binarySearch(A, n, x)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestValueNotFound(t *testing.T) {
+	
+	A := make([]int, 100)
+	n := len(A)
+	x := -1
+
+	// create an array to search
+	for i := 0; i < n; i++ {
+		A[i] = i
+	}
+
+	// find our value	
+	_, err := binarySearch(A, n, x)
+	if err == nil {
+		t.Fail()
+	}
+}
