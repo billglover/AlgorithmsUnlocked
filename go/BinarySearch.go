747d3b869beca9447bb62c78b4001d9df2a959c0
Bill Glover <bill@billglover.co.uk>
2016-06-10 07:13:47 +0100

    Remove all tests from main function.

--- go/BinarySearch.go
+++ go/BinarySearch.go
@@ -3,8 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func binarySearch(A []int, n int, x int) (int, error) {
@@ -29,34 +27,9 @@ func binarySearch(A []int, n int, x int) (int, error) {
 	return iterations, errors.New("not found")
 }
 
-func findRandomValueInArray(A []int, n int) int {
-	randomValue := rand.Intn(n)
-	iterations, _ := binarySearch(A, n, randomValue)
-	return iterations
-}
-
 func main() {
-	// seed the random number gnerator and log the seed
-	seed := time.Now().UTC().UnixNano()
-	rand.Seed(seed)
-	fmt.Printf("Random seed: %d\n", seed)
-
-	// generate array to search
-	fmt.Printf("Generating array to search...")
-	A := make([]int, 1000000000)
-	for i := 0; i < len(A); i++ {
-		A[i] = i
-	}
-	fmt.Printf("DONE\n")
-
-	fmt.Printf("\nArray Size | Iterations\n")
-
-	for n := 10; n <= len(A); n = n * 10 {
-		sumOfIterations := 0
-		for i := 0; i < 1000; i++ {
-			sumOfIterations += findRandomValueInArray(A, n)
-		}
-		avgIterations := sumOfIterations / 1000
-		fmt.Printf("%10d | %9d\n", n, avgIterations)
-	}
+	fmt.Println("To demonstrate Binary Search, please execute the tests.\n")
+	fmt.Println("\tgo test -v ./....\n")
+	fmt.Println("Or use the -bench flag to include benchmarks.\n")
+	fmt.Println("\tgo test -bench . -v ./....\n")
 }
